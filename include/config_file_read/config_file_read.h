197026c9bd606f5fde5c1f3d849fd8a36feef4f0
ktada <kota.tada@soliton.co.jp>
2018-01-19 13:08:36 +0900

    fixes #6 クラス化をやめた

--- include/config_file_read/config_file_read.h
+++ include/config_file_read/config_file_read.h
@@ -2,9 +2,5 @@
 #ifndef CONFREAD_CONFIGFILEREAD_H_  // NOLINT
 #define CONFREAD_CONFIGFILEREAD_H_
 #include <string>
-class ConfigFileRead {
- public:
-  std::string GetConfigString(const std::string key, const std::string inifile);
-};
-
+std::string GetConfigString(const std::string key, const std::string inifile);
 #endif  // CONFREAD_CONFIGFILEREAD_H_  //NOLINT
